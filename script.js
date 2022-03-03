e5349d9a876dfda8efb3fad249a81a6bc5a62c07
vanta blanta <vantablanta@gmail.com>
2022-03-03 07:19:47 +0300

    function calculate calls another helper add to run its addition section

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-
+//asking for user input
 let nameOfOperation = prompt("Name of operation i.e (addition, subtraction etc)").toUpperCase();
 let firstOperator = prompt("Enter first number");
 let operand = prompt("Enter the operand");
 let secondOperator = prompt("Enter second number");
 
+//linking to html 
 let heading = document.getElementById('a').textContent = `${nameOfOperation}`
 let num1 = document.getElementById('first-operand').textContent = `${firstOperator}`
 let sign = document.getElementById('operator').textContent = `${operand}`
 let num2 = document.getElementById('second-operand').textContent = `${secondOperator}`
+let result = document.getElementById('result')
+calculate()
 
+//the function doing its magic
 function calculate(){
-    let result = document.getElementById('result').textContent 
-    function add(){
+    function add(firstOperator, secondOperator){
         if (operand === '+'){
-        let ans = parseInt(firstOperator) + parseInt(secondOperator)
-
+        result.textContent = parseInt(firstOperator) + parseInt(secondOperator)
+        console.log(result)
         }
         else{
             alert("Incorrect sign for addition")
         }
     }
-    add();
+    add(firstOperator, secondOperator);
     /*function multiply(){
         //
     }
